80860ce791a3d3f08a631b8a6b222377d0153cd3
Omar Sainz <SQL_Slammer.exe@live.de>
2014-10-11 14:59:46 -0600

    Skills added

---
+++ portfolio/public/js/scripts.js
@@ -0,0 +1,13 @@
+$(document).on('ready', function(){
+	$(function() {
+    $('.chart').easyPieChart({
+      easing: 'easeOutBounce',
+      barColor: '#ddd',
+      size: 130,
+      lineWidth: 7,
+      onStep: function(from, to, percent) {
+        $(this.el).find('.percent').text(Math.round(percent));
+      }
+    });
+  }); 
+});
