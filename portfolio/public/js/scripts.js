811740219b8db77ae83840b39242762e0050e27b
Omar Sainz <SQL_Slammer.exe@live.de>
2014-10-12 11:42:15 -0600

    Responsive added with some details

--- portfolio/public/js/scripts.js
+++ portfolio/public/js/scripts.js
@@ -1,4 +1,21 @@
+var arrowDown   = $('#arrow-down');
+var navbar      = $('.navbar-default');
+
 $(document).on('ready', function(){
+	var scroll_pos = 0;
+	$(document).scroll(function(){
+		scroll_pos = $(this).scrollTop();
+		if(scroll_pos > 800){
+			navbar.addClass('navbar-fixed-top');
+		}
+		else {
+			navbar.removeClass('navbar-fixed-top');
+		}
+	});
+
+	arrowDown.on('click', function() {
+		$('body').animate({ scrollTop: 800}, 'slow');
+	});
 	$(function() {
     $('.chart').easyPieChart({
       easing: 'easeOutBounce',
