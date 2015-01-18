0b14910e1bd86cdae68998feb175849d87fffbf0
Omar Sainz <SQL_Slammer.exe@live.de>
2015-01-17 18:36:47 -0600

    -Folders removed -Ekcos removed -Video files removed

--- escko/app/js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).on('ready', function (){ 
-	console.log('Hola');
-	$('.brand').addClass('animated fadeInRight ');
-	$('.studio').addClass('animated fadeInLeft');
-	$();
-	/*$('.divider').addClass('animated fadeOutRight');*/
-});
