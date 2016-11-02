e0c4300cdca71745ba6d29c71cd372e2b824ec96
Omar Sainz <SQL_Slammer.exe@live.de>
2016-11-02 10:10:33 -0600

    Slider modified, new styles and images added to Angelis project

--- angelis/js/main.js
+++ angelis/js/main.js
@@ -1,5 +1,34 @@
+var navbar       = $('.navbar-default');
+var arrowDown    = $('#arrow-down');
+var footer       = $('.footer--container');
+
 $(document).ready(function() {
 	console.log('Hello World');
+	var scroll_pos = 0;
+	$(document).scroll(function(){
+		scroll_pos = $(this).scrollTop();
+		if(scroll_pos > 750){
+			navbar.addClass('navbar-fixed-top');
+			navbar.css('margin-top', '0');
+			footer.css({
+				'display': 'block'
+			});
+			footer.addClass('navbar-fixed-bottom');
+		}
+		else {
+			navbar.removeClass('navbar-fixed-top');
+			navbar.css('margin-top', '-3rem');
+			footer.css({
+				'display': 'none'
+			});
+			footer.removeClass('navbar-fixed-bottom');
+		}
+	});
+
+	arrowDown.on('click', function() {
+		$('body').animate({ scrollTop: 800}, 'slow');
+	});
+
 	$('.grid').masonry({
   // set itemSelector so .grid-sizer is not used in layout
 	  itemSelector: '.grid-item',
