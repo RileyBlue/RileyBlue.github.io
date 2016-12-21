b0cae0626dfc0daa52c1f58573db76fcfe78496c
Omar Sainz <SQL_Slammer.exe@live.de>
2016-12-20 23:15:41 -0700

    Changes for RWD in JS

--- angelis/js/main.js
+++ angelis/js/main.js
@@ -39,25 +39,64 @@ $(document).ready(function() {
 	    }
 	});
 
-	arrowDown.on('click', function() {
-		$('body').animate({ scrollTop: 550}, 'slow');
-	});
+	var mediaquery = window.matchMedia("(max-width: 570px)");
+	if (mediaquery.matches) {
+		var scroll_pos = 0;
+		$(document).scroll(function(){
+			scroll_pos = $(this).scrollTop();
+			if(scroll_pos > 0){
+				navbar.addClass('navbar-fixed-top');
+				navbar.css('margin-top', '0');
+				footer.css({
+					'display': 'block'
+				});
+				footer.addClass('navbar-fixed-bottom');
+			}
+			else {
+				navbar.removeClass('navbar-fixed-top');
+				/*navbar.css('margin-top', '-3rem');*/
+				footer.css({
+					'display': 'none'
+				});
+				footer.removeClass('navbar-fixed-bottom');
+			}
+		});
+		servicesLink.on('click', function() {
+			$('body').animate({ scrollTop: 470}, 'slow');
+		});
 
-	servicesLink.on('click', function() {
-		$('body').animate({ scrollTop: 550}, 'slow');
-	});
+		galleryLink.on('click', function() {
+			$('body').animate({ scrollTop: 1090}, 'slow');
+		});
 
-	galleryLink.on('click', function() {
-		$('body').animate({ scrollTop: 1200}, 'slow');
-	});
+		faqLink.on('click', function() {
+			$('body').animate({ scrollTop: 3060}, 'slow');
+		});	
 
-	faqLink.on('click', function() {
-		$('body').animate({ scrollTop: 2010}, 'slow');
-	});	
+		contactLink.on('click', function() {
+			$('body').animate({ scrollTop: 3580}, 'slow');
+		});	
+	} else {
+	  	arrowDown.on('click', function() {
+			$('body').animate({ scrollTop: 550}, 'slow');
+		});
 
-	contactLink.on('click', function() {
-		$('body').animate({ scrollTop: 2500}, 'slow');
-	});	
+		servicesLink.on('click', function() {
+			$('body').animate({ scrollTop: 550}, 'slow');
+		});
+
+		galleryLink.on('click', function() {
+			$('body').animate({ scrollTop: 1200}, 'slow');
+		});
+
+		faqLink.on('click', function() {
+			$('body').animate({ scrollTop: 2010}, 'slow');
+		});	
+
+		contactLink.on('click', function() {
+			$('body').animate({ scrollTop: 2500}, 'slow');
+		});	
+	}
 
 	$('.grid').masonry({
   // set itemSelector so .grid-sizer is not used in layout
