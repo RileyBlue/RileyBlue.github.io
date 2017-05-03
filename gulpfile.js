7b1ef543ecb7d0a19239a52163e926fad610b54e
Omar Sainz <SQL_Slammer.exe@live.de>
2017-05-03 04:27:08 +0000

    Gulp modified for Sass tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 'use strict';
 
-const path = require('path');
-const gulp = require('gulp');
+const path   = require('path');
+const gulp   = require('gulp');
 const concat = require('gulp-concat');
+const sass   = require('gulp-sass');
 const uglify = require('gulp-uglify');
 const minifyCSS = require('gulp-clean-css');
-const del = require('del');
-const shell = require('gulp-shell');
+const del    = require('del');
+const shell  = require('gulp-shell');
 
 /**
  * Production css file
@@ -32,6 +33,14 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest(DIST_FOLDER));
 });
 
+// Sass Files 
+gulp.task('sass', function () {
+  gulp.src(['./sass/base.scss'])
+    .pipe(concat(CSS_DIST))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(DIST_FOLDER));
+});
+
 // Remove all js files from the production folder
 gulp.task('clean-js', function() {
   del([`${DIST_FOLDER}*.js`, ]).then(paths => {
@@ -41,10 +50,11 @@ gulp.task('clean-js', function() {
 
 // Remove all css files from the production folder
 gulp.task('clean-css', function() {
-  // Task
+  del([`${DIST_FOLDER}*.css`, ]).then(paths => {
+    paths.length && console.log('Removed:\n', paths.join('\n'));
+  });
 });
 
-
-gulp.task('build', ['clean-js', 'scripts']);
+gulp.task('build', ['clean-js', 'clean-css', 'scripts', 'sass']);
 
 gulp.task('deploy', ['build']);
